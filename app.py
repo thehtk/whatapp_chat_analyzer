547d3f5bea24ccc057acb9cea30507e67d8de4aa
HITESH KUMAR <73693548+thehtk@users.noreply.github.com>
2023-07-11 00:40:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,9 +114,9 @@ if uploaded_file is not None:
 
         st.title('Most commmon words')
         st.pyplot(fig)
-"""
+
         # emoji analysis
-        """
+        
         emoji_df = helper.emoji_helper(selected_user,df)
         st.title("Emoji Analysis")
 
@@ -127,7 +127,7 @@ if uploaded_file is not None:
         with col2:
             fig,ax = plt.subplots()
             ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
-            st.pyplot(fig)"""
+            st.pyplot(fig) """
 
 
 
